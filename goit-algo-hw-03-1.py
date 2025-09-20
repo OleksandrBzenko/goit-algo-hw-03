215d9de341b23b0571b0ca628635cccf2f32aae1
Oleksadr <aleksandrbzenko8@gmail.com>
2025-09-20 13:33:58 +0200

    Update goit-algo-hw-03-1.py

--- goit-algo-hw-03-1.py
+++ goit-algo-hw-03-1.py
@@ -1,11 +1,16 @@
 from datetime import datetime, date
 
 
-def get_day_of_today(day_str):
-    between_day = datetime.strptime(day_str, "%Y-%m-%d").date()
-    return(between_day - date.today()).days
-print(get_day_of_today("1989-08-02"))
-print(get_day_of_today("2025-09-29"))
+def get_days_from_today(day_str):
+
+    try:
+        between_day = datetime.strptime(day_str, "%Y-%m-%d").date()
+        return(between_day - date.today()).days
+    except ValueError:
+        return "Invalid date format. Please use 'YYYY-MM-DD'."
+print(get_days_from_today("1989-08-02"))
+print(get_days_from_today("2025-09-29"))
+print(get_days_from_today("12-10-2027"))
     
     
 
