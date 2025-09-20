1e98bdef83305527208fe51d1e5a023ec8077a0a
Oleksadr <aleksandrbzenko8@gmail.com>
2025-09-20 13:35:27 +0200

    Update goit-algo-hw-03-2.py

--- goit-algo-hw-03-2.py
+++ goit-algo-hw-03-2.py
@@ -2,10 +2,11 @@ import random
 
 
 def get_numbers_ticket(min_num, max_num, quantity):
-    if quantity>(max_num - min_num +1):
-        raise ValueError("Qantity is larger tham range of numbers")
-    return random.sample(range (min_num, max_num +1), quantity)
+    if min_num < 1 or max_num > 1000 or quantity > (max_num - min_num + 1):
+     return [] 
+    return sorted(random.sample(range (min_num, max_num +1), quantity))
 print("Your lottery ticket:", get_numbers_ticket(1, 49, 6))
 print("Your lottery ticket:", get_numbers_ticket(1, 36, 5))
+print("Your lottery ticket:", get_numbers_ticket(10, 4, 5))
 
 
